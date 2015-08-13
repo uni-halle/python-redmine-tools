b3358bc480bb84f9035cd162650e74da46312e7e
David Lukas Müller <dlmueller@Davids-MacBook-Pro.local>
2015-08-13 14:14:15 +0200

    cross-project index can now be written directly to the target wiki page

--- overall_index.py
+++ overall_index.py
@@ -5,6 +5,7 @@
 #--- Python
 import sys
 import StringIO
+import argparse
 
 #---
 #--- 3rd party
@@ -134,27 +135,41 @@ class PageTree(object) :
             yield page
 
 #---
-def printGlobalIndex(baseURL, apiKey) :
-    r = Redmine(baseURL, key = apiKey)
+def printGlobalIndex(redmineHandle) :
+    lineCount = 0
+    for line in iterGlobaleIndexLines(redmineHandle) :
+        lineCount += 1
+        print line
+    return lineCount
+
+def iterGlobaleIndexLines(redmineHandle, **keywords) :
+    """
+    Iterates over the lines of the target document line by line.
+
+    @keyword printProgress: If True progress information will be printed to stdout
+    @type    printProgress: bool
+    """
+    printProgress = keywords.get('printProgress', False)
+    baseURL = redmineHandle.url
+    r = redmineHandle
 
     allProjects = r.project.all()
     projectCount = len(allProjects)
     if projectCount == 0 :
-        return 0
+        return
 
     projectTree = ProjectTree(allProjects)
 
     FIRST_TIME = True
-    pageCount = 0
 
-    for project in projectTree.iter_dfs() :
+    for (projNum, project) in enumerate(projectTree.iter_dfs()) :
         breadcrumbTrail = " >> ".join(projectTree.getBreadcrumbTrail(project))
         pid = project.id # numeric
         projectIdent = project.identifier.encode('utf-8', 'ignore') # symbolic
         pname = project.name
 
         # singlePage = r.wiki_page.get('Mitarbeiter', project_id = PID)
-        # print singlePage.text
+        # yield singlePage.text
 
         allPagesQuery =  r.wiki_page.filter(project_id = pid)
         try :
@@ -163,21 +178,23 @@ def printGlobalIndex(baseURL, apiKey) :
             #print "Cannot access pages of project '%(pident)s'!" % locals()
             allPages = []
         if len(allPages) > 0 :
-
+            if printProgress :
+                print "%i/%i:" %(projNum+1, projectCount),
             if FIRST_TIME :
-                print "{{>toc}}"
-                print ""
-                print "h1. Global Index"
-                print ""
+                yield "{{>toc}}"
+                yield ""
+                yield "h1. Global Index"
+                yield ""
                 FIRST_TIME = False
 
-            print ""
-            print "h2. %(breadcrumbTrail)s" % locals()
-            print ""
+            yield ""
+            yield "h2. %(breadcrumbTrail)s" % locals()
+            yield ""
 
             pageTree = PageTree(project, allPages)
-            for page in pageTree.iter_dfs() :
-                pageCount +=1
+            for (pageNum,page) in enumerate(pageTree.iter_dfs()) :
+                if printProgress :
+                    print ".",
 
                 pageTitle = page.title.encode('utf-8', 'ignore')
                 prettyPageTitle = pageTitle.replace('_', ' ')
@@ -187,28 +204,98 @@ def printGlobalIndex(baseURL, apiKey) :
                 ancestors = pageTree.getAncestorsAndPage(page) # getAncestorPages(page)
                 indentCount = len(ancestors) + 1
                 indent = "*" * indentCount
-                print "%(indent)s [[%(projectIdent)s:%(prettyPageTitle)s]]" % locals()
-                # print "%(indent)s %(pageTitle)s (von %(pageAuthor)s) -> %(ancestors)r" % locals()
+                yield "%(indent)s [[%(projectIdent)s:%(prettyPageTitle)s]]" % locals()
+                # yield "%(indent)s %(pageTitle)s (von %(pageAuthor)s) -> %(ancestors)r" % locals()
 
-            print ""
-            print '"Hauptseite":%(baseURL)s/projects/%(projectIdent)s/wiki' % locals()
-            print '"Seiten nach Titel sortiert":%(baseURL)s/projects/%(projectIdent)s/wiki/index' % locals()
-            print '"Seiten nach Datum sortiert":%(baseURL)s/projects/%(projectIdent)s/wiki/date_index' % locals()
+            if printProgress :
+                print ""
 
+            yield ""
+            yield '"Hauptseite":%(baseURL)s/projects/%(projectIdent)s/wiki' % locals()
+            yield '"Seiten nach Titel sortiert":%(baseURL)s/projects/%(projectIdent)s/wiki/index' % locals()
+            yield '"Seiten nach Datum sortiert":%(baseURL)s/projects/%(projectIdent)s/wiki/date_index' % locals()
+
+#---
+class CLI(object) :
+    """
+    Encapsulates the Command Line Interface.
+    """
+    def __init__(self) :
+        self._parser = parser = self._createParser()
+        self._args = args = parser.parse_args()
+        if args.apikey is None :
+            print "You must provide a Redmine API-Key as first argument."
+            sys.exit(1)
+            return
+
+    def _createParser(self) :
+        parser = argparse.ArgumentParser()
+        parser.add_argument("--apikey", help = "Valid API-key to use the Python REST-API")
+        parser.add_argument("-t", "--targetpage",
+                            help = "Fully qualified Name of a Wiki page the result should be stored on",
+                            default = "")
+        parser.add_argument("-p", "--projectid",
+                            help = "Id of the project the target wiki page belongs to",
+                            default = "")
+
+        return parser
+
+
+    def getBaseURL(self) :
+        """
+        Base URL of the Redmine instance.
+        """
+        baseURL = 'https://redmine.itz.uni-halle.de'
+        return baseURL
+
+    def getApiKey(self) :
+        """
+        Valid API-key to use the REST-API of Redmine.
+        """
+        return self._args.apikey
+
+    def getTargetPage(self) :
+        """
+        @rtype: None or str
+        """
+        return self._args.targetpage
+
+    def getProjectId(self) :
+        return self._args.projectid
 
 #---
 def main() :
-    baseURL = 'https://redmine.itz.uni-halle.de'
-    try :
-        apiKey = sys.argv[1]
-    except IndexError :
-        print "You must provide a Redmine API-Key as first argument."
-        sys.exit(1)
+    cli = CLI()
+    baseURL = cli.getBaseURL()
+    apiKey = cli.getApiKey()
 
-    pageCount = printGlobalIndex(baseURL, apiKey)
+    redmineHandle = Redmine(baseURL, key = apiKey)
 
-    if pageCount == 0 :
+
+    allProjects = redmineHandle.project.all()
+    projectCount = len(allProjects)
+    if projectCount == 0 :
         print "You must provide a VALID Redmine API-Key!"
+        return
+
+    targetPage = cli.getTargetPage()
+    projectId = cli.getProjectId()
+    print projectId, targetPage
+    if not targetPage or not projectId:
+        lineCount = printGlobalIndex(redmineHandle)
+    else :
+        pageLines = list(iterGlobaleIndexLines(redmineHandle, printProgress = True))
+        newText = "\n".join(pageLines)
+        oldPage = redmineHandle.wiki_page.get(targetPage, project_id=projectId)
+        oldText = oldPage.text.encode('utf-8')
+        if oldText != newText :
+            redmineHandle.wiki_page.update(targetPage,
+                                           project_id = projectId,
+                                           title = 'Global index',
+                                           text = newText,
+                                           parent_title ='',
+                                           comments = 'automatisch aktualisiert')
+
     return
 
 if __name__ == '__main__' :
